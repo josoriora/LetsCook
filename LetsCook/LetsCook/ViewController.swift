3a88c6ef2cce7e19a1e823f96185774568804d42
josoriora <julian.osorior@gmail.com>
2020-04-21 19:23:30 -0500

    - Initial project set up.

---
+++ LetsCook/LetsCook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LetsCook
+//
+//  Created by Julian Andres Osorio Ramirez on 4/21/20.
+//  Copyright © 2020 julian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
